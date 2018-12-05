1a6bdf3b99b783b56cd5d64651bb87149c787827
ProgramistycznySwir <39069042+ProgramistycznySwir@users.noreply.github.com>
2018-12-05 16:08:17 +0100

    version 0.1.1.1
    made Rocket_AI super easy way

--- Assets/Scripts/Display_Stats.cs
+++ Assets/Scripts/Display_Stats.cs
@@ -27,10 +27,12 @@ public class Display_Stats : MonoBehaviour
     void Update()
     {
         text.text = Convert.ToString(Convert.ToInt16( shipWhichSpeedDisplay.GetComponent<Rigidbody2D>().velocity.magnitude * 100));
-        ammoText.text = Convert.ToString(shipWhichSpeedDisplay.transform.GetChild(2).GetComponent<Turret_Control>().rounds);
+        if(shipWhichSpeedDisplay.transform.GetChild(2).GetComponent<Turret_Control>()) ammoText.text = Convert.ToString(shipWhichSpeedDisplay.transform.GetChild(2).GetComponent<Turret_Control>().rounds);
+        else if(shipWhichSpeedDisplay.transform.GetChild(2).GetComponent<Rocket_Launcher_Script>()) ammoText.text = Convert.ToString(shipWhichSpeedDisplay.transform.GetChild(2).GetComponent<Rocket_Launcher_Script>().rounds);
         if (shipWhichSpeedDisplay.GetComponent<Rigidbody2D>().velocity.magnitude > shipWhichSpeedDisplay.GetComponent<Stats>().maxVelocity) text.color = speedWarningColor;
         else text.color = basicTextColor;
         speedMeter.fillAmount = shipWhichSpeedDisplay.GetComponent<Rigidbody2D>().velocity.magnitude / (shipWhichSpeedDisplay.GetComponent<Stats>().maxVelocity *2);
-        ammoBar.fillAmount = Convert.ToSingle(shipWhichSpeedDisplay.transform.GetChild(2).GetComponent<Turret_Control>().rounds) / Convert.ToSingle(shipWhichSpeedDisplay.transform.GetChild(2).GetComponent<Turret_Control>().magazineSize);
+        if(shipWhichSpeedDisplay.transform.GetChild(2).GetComponent<Turret_Control>()) ammoBar.fillAmount = Convert.ToSingle(shipWhichSpeedDisplay.transform.GetChild(2).GetComponent<Turret_Control>().rounds) / Convert.ToSingle(shipWhichSpeedDisplay.transform.GetChild(2).GetComponent<Turret_Control>().magazineSize);
+        else if (shipWhichSpeedDisplay.transform.GetChild(2).GetComponent<Rocket_Launcher_Script>()) ammoBar.fillAmount = Convert.ToSingle(shipWhichSpeedDisplay.transform.GetChild(2).GetComponent<Rocket_Launcher_Script>().rounds) / Convert.ToSingle(shipWhichSpeedDisplay.transform.GetChild(2).GetComponent<Rocket_Launcher_Script>().magazineSize);
     }
 }
--- Assets/Scripts/Rocket_AI.cs
+++ Assets/Scripts/Rocket_AI.cs
@@ -21,21 +21,15 @@ public class Rocket_AI : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector2 directionV2 = new Vector2(target.position.x - transform.position.x, target.position.y - transform.position.y);
-        float dirrection = -((Mathf.Atan2(directionV2.x, directionV2.y) * 180) / Mathf.PI) - 90;
-        if (dirrection > 180) dirrection -= 360;
-        else if (dirrection < -180) dirrection += 360;
-        float courseChange = dirrection - transform.eulerAngles.z;
-            //transform.eulerAngles = new Vector3(0, 0, -((Mathf.Atan2(forceDirrection.x, forceDirrection.y) * 180) / Mathf.PI) - 90);
-        if (0 > dirrection)
-        {
-            engines.TurnLeft();
-        }
-        if (0 < dirrection)
-        {
-            engines.TurnRight();
-        }
-        Debug.Log("dirrection: " + dirrection + " transform: " + transform.eulerAngles.z + " course change: " + courseChange);
+        Vector2 dirrection = new Vector2(target.position.x - transform.position.x, target.position.y - transform.position.y);
+        //float dirrection = -((Mathf.Atan2(directionV2.x, directionV2.y) * 180) / Mathf.PI) - 90;
+        //if (dirrection > 180) dirrection -= 360;
+        //else if (dirrection < -180) dirrection += 360;
+        //float courseChange = dirrection - transform.eulerAngles.z;
+        //transform.eulerAngles = new Vector3(0, 0, -((Mathf.Atan2(forceDirrection.x, forceDirrection.y) * 180) / Mathf.PI) - 90);
+        transform.up = dirrection.normalized;
+        engines.Accelerate();
+        Debug.Log("dirrection: " + dirrection + " transform: " + transform.eulerAngles.z/* + " course change: " + courseChange*/);
     }
 
     public void GiveTarget(Transform target)
